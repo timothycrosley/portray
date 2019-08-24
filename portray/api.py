5d23251c542267830ca3bbe6352a85ef4688cb5c
Timothy Crosley <timothy.crosley@gmail.com>
2019-08-24 13:03:29 -0700

    Add in_browser option

--- portray/api.py
+++ portray/api.py
@@ -1,4 +1,5 @@
 import os
+import webbrowser
 
 import hug
 
@@ -16,7 +17,7 @@ def read_config(directory: str = os.getcwd(), config_file: str = "pyproject.toml
 
 
 def server(
-    directory: str = os.getcwd(), config_file: str = "pyproject.toml", output_dir: str = "site"
+    directory: str = os.getcwd(), config_file: str = "pyproject.toml", open_browser: bool = False
 ):
     api = hug.API("Doc Server")
 
@@ -27,9 +28,21 @@ def server(
         def my_static_dirs():
             return (doc_folder,)
 
+        if open_browser:
+
+            @hug.startup(api=api)
+            def open_browser_to_docs(*args, **kwargs):
+                webbrowser.open_new("{}:{}".format(project_config["host"], project_config["port"]))
+
         api.http.serve(
             host=project_config["host"],
             port=project_config["port"],
             no_documentation=True,
             display_intro=False,
         )
+
+
+def in_browser(
+    directory: str = os.getcwd(), config_file: str = "pyproject.toml", output_dir: str = "site"
+):
+    return server(directory=directory, config_file=config_file, open_browser=True)
