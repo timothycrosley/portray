71048dc76495672d174d2c093b6931efc69eca4f
Timothy Crosley <timothy.crosley@gmail.com>
2019-08-25 11:21:40 -0800

    Deployed  with MkDocs version: 1.0.4

--- site/scripts/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -xe
-
-poetry run isort --recursive portray/ tests/
-poetry run black portray/ tests/ -l 100
--- site/scripts/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -xe
-
-./scripts/lint.sh
-poetry run pytest -s --cov=portray --cov=tests --cov-report=term-missing ${@}
