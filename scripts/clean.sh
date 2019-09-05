19cc15fa5037ac69bec41a8c0f3b98e9950ba579
Timothy Crosley <timothy.crosley@gmail.com>
2019-09-04 21:26:51 -0700

    Ensure black compatible settings are supplied to isort for both formatting and linting

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 
-poetry run isort --recursive portray/ tests/
+poetry run isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --use-parentheses --line-width=100 --recursive portray/ tests/
 poetry run black portray/ tests/ -l 100
