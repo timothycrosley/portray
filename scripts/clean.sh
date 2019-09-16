fdfedbb2c9ac2078d85792a61c1937d1e2e3b3ae
Timothy Crosley <timothy.crosley@gmail.com>
2019-09-15 18:38:58 -0800

    Deployed  with MkDocs version: 1.0.4

--- scripts/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -xe
-
-poetry run isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --use-parentheses --line-width=100 --recursive portray/ tests/
-poetry run black portray/ tests/ -l 100
