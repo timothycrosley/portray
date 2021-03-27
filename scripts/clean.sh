6f96306005d227af95d386485296983906a28046
Timothy Crosley <timothy.crosley@gmail.com>
2021-03-26 22:32:10 -0700

    Update isort

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 
-poetry run isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --use-parentheses --line-width=100 --recursive portray/ tests/
+poetry run isort --profile hug portray/ tests/
 poetry run black portray/ tests/ -l 100
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 poetry run mypy --ignore-missing-imports portray/
-poetry run isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --use-parentheses --line-width=100 --recursive --check --diff --recursive portray/ tests/
+poetry run isort --profile hug --check --diff portray/ tests/
 poetry run black --check -l 100 portray/ tests/
 poetry run flake8 portray/ tests/ --max-line 100 --ignore F403,F401,W503
 poetry run safety check -i 39462
